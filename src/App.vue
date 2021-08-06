955a7418ad17ac737b11f8942d2b41678500fedb
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-08-06 17:08:37 +0800

    初步完成界面布局样式

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,65 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+    <v-app id="inspire">
+        <v-app-bar color="pink lighten-2" dark app>
+            <v-toolbar-title>{{ titleName }}</v-toolbar-title>
+        </v-app-bar>
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+        <v-bottom-navigation v-model="selectedItem" :background-color="color" dark shift app>
+            <v-btn :to="item.to" v-for="(item, i) in items" :key="i">
+                <span>{{ item.text }}</span>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-btn>
+        </v-bottom-navigation>
+    </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+    data() {
+        return {
+            selectedItem: 0,
+            items: [
+                {text: '练习', icon: 'mdi-school', to: '/'},
+                {text: '考试', icon: 'mdi-ab-testing', to: '/about'},
+                {text: '设置', icon: 'mdi-cog', to: '/settings'},
+            ],
+        }
+    },
+    computed: {
+        // 底部导航栏颜色
+        color() {
+            switch (this.selectedItem) {
+                case 0:
+                    return 'blue-grey'
+                case 1:
+                    return 'teal'
+                case 2:
+                    return 'brown'
+                case 3:
+                    return 'indigo'
+                default:
+                    return 'blue-grey'
+            }
+        },
+        titleName() {
+            switch (this.$route.name) {
+                case 'Home':
+                    return '在线培训系统'
+                case 'About':
+                    return '考试'
+                case 'Settings':
+                    return '设置'
+                default:
+                    return ''
+            }
+        }
+    },
+    methods: {
+        changeTitle(name) {
+            this.$store.commit("setToolbarTitleName", name)
+        }
+    }
+}
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        toolbarTitleName: ""
+    },
+    mutations: {
+        // 设置顶部状态栏标题
+        setToolbarTitleName(state, name) {
+            state.toolbarTitleName = name
+        }
+    },
+    actions: {},
+    modules: {}
 })
