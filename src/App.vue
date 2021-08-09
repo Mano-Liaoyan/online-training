fb5f763b3a15889d7ed183c720a56d685102b233
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-08-09 21:02:01 +0800

    侧边导航栏完善

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,23 @@
             </v-btn>
             <v-toolbar-title class="pl-0">{{ titleName }}</v-toolbar-title>
         </v-app-bar>
-        <v-navigation-drawer clipped app class="pt-4"
-                             color="grey lighten-3" mini-variant>
-            <v-avatar v-for="n in 6" :key="n" :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
-                      :size="n === 1 ? 36 : 20" class="d-block text-center mx-auto mb-9"></v-avatar>
+        <v-navigation-drawer clipped app :color="color" dark mini-variant expand-on-hover hide-overlay touchless width="150">
+            <v-list flat>
+                <v-list-item-group v-model="selectedItem">
+                    <v-list-item v-for="(item, i) in items" :key="i" link :to="item.to">
+                        <v-list-item-icon>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title>{{ item.text }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
         </v-navigation-drawer>
         <!--  路由切换  -->
         <!--  路由间切换动画  -->
-        <v-container fluid>
+        <v-container>
             <v-main>
                 <v-slide-x-transition mode="out-in">
                     <router-view/>
@@ -22,7 +31,7 @@
             </v-main>
         </v-container>
         <!--  底部导航栏  -->
-        <v-bottom-navigation v-show="this.$route.name !== 'Login' && isMobileDevice"
+        <v-bottom-navigation v-if="this.$route.name !== 'Login' && isMobileDevice"
                              v-model="selectedItem" :background-color="color" dark shift app>
             <v-btn :to="item.to" v-for="(item, i) in items" :key="i">
                 <span>{{ item.text }}</span>
@@ -48,17 +57,18 @@ export default {
     computed: {
         // 底部导航栏颜色
         color() {
+            console.log(this.selectedItem)
             switch (this.selectedItem) {
                 case 0:
-                    return 'blue-grey'
+                    return 'pink lighten-3'
                 case 1:
-                    return 'teal'
+                    return 'red lighten-3'
                 case 2:
-                    return 'brown'
+                    return 'blue lighten-3'
                 case 3:
-                    return 'indigo'
+                    return 'indigo lighten-3'
                 default:
-                    return 'blue-grey'
+                    return 'deep-purple lighten-3'
             }
         },
         // 顶部标题栏文字
