f2225bf7ceca89332f837c4b85d547c17152e4b3
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-08-09 20:04:30 +0800

    判断是否为移动设备

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
     <v-app id="inspire">
         <!--  头部状态栏  -->
-        <v-app-bar :color="color" dark app elevation="0">
+        <v-app-bar :color="color" dark app elevation="0" clipped-left>
             <v-btn v-show="showBack" icon @click="$router.back()">
                 <v-icon>mdi-arrow-left</v-icon>
             </v-btn>
             <v-toolbar-title class="pl-0">{{ titleName }}</v-toolbar-title>
         </v-app-bar>
+        <v-navigation-drawer clipped app class="pt-4"
+                             color="grey lighten-3" mini-variant>
+            <v-avatar v-for="n in 6" :key="n" :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
+                      :size="n === 1 ? 36 : 20" class="d-block text-center mx-auto mb-9"></v-avatar>
+        </v-navigation-drawer>
         <!--  路由切换  -->
-        <v-main>
-            <!--  路由间切换动画  -->
-            <v-container fluid class="pa-2">
+        <!--  路由间切换动画  -->
+        <v-container fluid>
+            <v-main>
                 <v-slide-x-transition mode="out-in">
                     <router-view/>
                 </v-slide-x-transition>
-            </v-container>
-        </v-main>
+            </v-main>
+        </v-container>
         <!--  底部导航栏  -->
-        <v-bottom-navigation v-if="this.$route.name !== 'Login'" v-model="selectedItem" :background-color="color" dark shift app>
+        <v-bottom-navigation v-show="this.$route.name !== 'Login' && isMobileDevice"
+                             v-model="selectedItem" :background-color="color" dark shift app>
             <v-btn :to="item.to" v-for="(item, i) in items" :key="i">
                 <span>{{ item.text }}</span>
                 <v-icon>{{ item.icon }}</v-icon>
@@ -55,6 +61,7 @@ export default {
                     return 'blue-grey'
             }
         },
+        // 顶部标题栏文字
         titleName() {
             switch (this.$route.name) {
                 case 'Home':
@@ -70,7 +77,23 @@ export default {
                 default:
                     return ''
             }
-        }
+        },
+        // 判断是否是移动设备 xs-sm-md 为移动设备
+        isMobileDevice() {
+            console.log(this.$vuetify.breakpoint.name)
+            switch (this.$vuetify.breakpoint.name) {
+                case 'xs':
+                    return true
+                case 'sm':
+                    return true
+                case 'md':
+                    return true
+                case 'lg':
+                    return false
+                case 'xl':
+                    return false
+            }
+        },
     },
     watch: {
         $route(now, old) {     //监控路由变换，控制返回按钮的显示
