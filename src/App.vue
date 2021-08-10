42d8f786202f77c49367077bf994f4822da74626
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-08-10 15:02:45 +0800

    优化底部动画

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
             <v-toolbar-title class="pl-0">{{ titleName }}</v-toolbar-title>
         </v-app-bar>
         <v-navigation-drawer clipped app :color="color" dark mini-variant expand-on-hover hide-overlay touchless width="150">
-            <v-list flat>
+            <v-list class="pt-0">
                 <v-list-item-group v-model="selectedItem">
                     <v-list-item v-for="(item, i) in items" :key="i" link :to="item.to">
                         <v-list-item-icon>
@@ -31,13 +31,18 @@
             </v-main>
         </v-container>
         <!--  底部导航栏  -->
-        <v-bottom-navigation v-if="this.$route.name !== 'Login' && isMobileDevice"
-                             v-model="selectedItem" :background-color="color" dark shift app>
-            <v-btn :to="item.to" v-for="(item, i) in items" :key="i">
-                <span>{{ item.text }}</span>
-                <v-icon>{{ item.icon }}</v-icon>
-            </v-btn>
-        </v-bottom-navigation>
+        <v-container>
+            <v-expand-transition mode="out-in">
+                <v-bottom-navigation
+                    v-if="this.$route.name !== 'Login' && isMobileDevice" grow
+                    v-model="selectedItem" :background-color="color" dark shift app>
+                    <v-btn :to="item.to" v-for="(item, i) in items" :key="i">
+                        <span>{{ item.text }}</span>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-btn>
+                </v-bottom-navigation>
+            </v-expand-transition>
+        </v-container>
     </v-app>
 </template>
 
@@ -57,7 +62,6 @@ export default {
     computed: {
         // 底部导航栏颜色
         color() {
-            console.log(this.selectedItem)
             switch (this.selectedItem) {
                 case 0:
                     return 'pink lighten-3'
