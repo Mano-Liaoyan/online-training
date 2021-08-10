38a4fad6576fa2b7fcad2f91b69d7beff330d0b8
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-08-10 15:11:15 +0800

    删除HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
-  <hello-world />
+    <v-container class="pa-0">
+        <v-row class="text-center" justify="center">
+            <v-col v-for="(item,idx) in 12" :key="idx" class="pb-0">
+                <TrainingCard></TrainingCard>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import TrainingCard from "@/components/TrainingCard";
 
-  export default {
+export default {
     name: 'Home',
-
     components: {
-      HelloWorld,
+        TrainingCard
     },
-  }
+    data() {
+        return {}
+    },
+}
 </script>
